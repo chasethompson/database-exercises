35c28aec4c6e17bcef6fe085c6388c645ef11f64
chasethompson <chase.w.thompson@gmail.com>
2020-02-07 14:50:26 -0600

    Adding tables exercise

---
+++ 3.3_tables_exercises.sql
@@ -0,0 +1,17 @@
+USE employees;
+
+SHOW TABLES;
+
+DESCRIBE employees;
+
+# 5. INT, DATE, VARCHAR, ENUM at the present data types
+
+# 6. If you do not include te PRIMARY KEY id, salaries
+
+# 7. All except the salaries table.
+
+# 8. The dept_emp, dept manager, employees, salaries, and titles
+
+# 9. The department only table has just dept numbers, and the employee table has a unquie id for each employee. The dept_emp table has a listing of the department associated with each employee, as does the employees_with_departments table. 
+
+SHOW CREATE TABLE dept_manager;
