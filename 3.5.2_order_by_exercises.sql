092576442af57439866e35548cd4a83b46883513
chasethompson <chase.w.thompson@gmail.com>
2020-02-10 14:16:50 -0600

    Adding SQL order by exercises

---
+++ 3.5.2_order_by_exercises.sql
@@ -0,0 +1,37 @@
+USE employees;
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name;
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name, last_name;
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name, first_name;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'e%'
+ORDER BY emp_no;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'e%e'
+ORDER BY emp_no ASC;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'e%e'
+ORDER BY emp_no DESC;
+
+SELECT *
+FROM `employees`
+WHERE birth_date LIKE '%12-25' AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+ORDER BY birth_date ASC, hire_date DESC;
+
