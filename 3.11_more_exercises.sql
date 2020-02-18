f106e1f566fa2b9f494f608485c6f406daa07c18
Chase Thompson <chase.w.thompson210@gmail.com>
2020-02-17 21:13:02 -0600

    Adding answers to SQL select, distinct, where and in exercises

--- 3.11_more_exercises.sql
+++ 3.11_more_exercises.sql
@@ -241,17 +241,339 @@ GROUP BY genre
 ORDER BY gross_revenue DESC
 LIMIT 5;
 
+-- 1. SELECT Statements
 
-/*
+# Select all columns from the actor table.
+
+SELECT
+	*
+FROM actor;
+
+# Select only the last_name column from the actor table.
+
+SELECT
+	last_name
+FROM actor;
+
+# Select only the following columns from the film table.
+
+SELECT
+	*
+FROM film;
+
+-- 2. DISTINCT Operator
+
+# Select all distinct (different) last names from the actor table.
+
+SELECT
+	DISTINCT last_name
+FROM actor;
+
+# Select all distinct (different) postal codes from the address table.
+
+SELECT
+	DISTINCT
+	postal_code
+FROM address;
+
+# Select all distinct (different) ratings from the film table.
+
+SELECT
+	DISTINCT
+	rating
+FROM film;
+
+-- 3. WHERE clause
+
+# Select the title, description, rating, movie length columns from the films table that last 3 hours or longer.
+
+SELECT
+	title,
+	description,
+	rating,
+	length
+FROM film
+WHERE length > 180;
+
+# Select the payment id, amount, and payment date columns from the payments table for payments made on or after 05/27/2005.
+
+SELECT
+	payment_id,
+	amount,
+	payment_date
+FROM payment
+WHERE payment_date > 2005/05/26;
+
+# Select the primary key, amount, and payment date columns from the payment table for payments made on 05/27/2005.
 
 
+SELECT
+	payment_id,
+	amount,
+	payment_date
+FROM payment
+WHERE payment_date > 2005/05/26; 
 
 
+# SELECT ALL COLUMNS FROM the customer TABLE FOR ROWS that have a LAST NAMES beginning WITH S AND a FIRST NAMES ending WITH N.
+
+SELECT
+	*
+FROM customer
+WHERE last_name LIKE "s%" AND first_name LIKE "%n";
+
+# SELECT ALL COLUMNS FROM the customer TABLE FOR ROWS WHERE the customer IS inactive OR has a LAST NAME beginning WITH "M".
+
+SELECT *
+	FROM customer
+	WHERE last_name LIKE 'm%' OR active = 0;
 
 
+# SELECT ALL COLUMNS FROM the category TABLE FOR ROWS WHERE the PRIMARY KEY IS greater THAN 4 AND the NAME field begins WITH either C, S OR T.
+
+SELECT
+	*
+FROM category
+WHERE category_id  > 4 AND NAME LIKE "C%" OR NAME LIKE "S%" OR NAME LIKE "T%";
+
+# Regexp solution
+
+SELECT
+	*
+FROM category
+WHERE category_id > 4
+AND NAME REGEXP '^[cst]';
+
+# SELECT ALL COLUMNS minus the PASSWORD COLUMN FROM the staff TABLE FOR ROWS that contain a password.
+
+SELECT
+	staff_id,
+	first_name,
+	last_name,
+	address_id,
+	picture,
+	email,
+	store_id,
+	active,
+	username,
+	last_update
+FROM staff
+WHERE PASSWORD IS NOT NULL; # Also think creating a temp table here, then dropping the column not wante whould work too
+
+# SELECT ALL COLUMNS minus the PASSWORD COLUMN FROM the staff TABLE FOR ROWS that DO NOT contain a password.
+
+SELECT
+	staff_id,
+	first_name,
+	last_name,
+	address_id,
+	picture,
+	email,
+	store_id,
+	active,
+	username,
+	last_update
+FROM staff
+WHERE PASSWORD IS NULL;
+
+-- 4. IN operator
+
+# SELECT the phone AND district COLUMNS FROM the address TABLE FOR addresses IN California, England, Taipei, OR West Java.
+
+SELECT
+	phone,
+	district
+FROM address
+WHERE district IN ('California', 'England', 'Taipei', 'West Java');
+
+# SELECT the payment id, amount, AND payment DATE COLUMNS FROM the payment TABLE FOR payments made ON 05/25/2005, 05/27/2005, AND 05/29/2005. (USE the IN operator AND the DATE FUNCTION, instead of the AND operator AS IN previous exercises.)
+
+SELECT
+	payment_id,
+	amount,
+	payment_date
+FROM payment
+WHERE DATE(payment_date)
+IN (DATE("2005-05-25"),DATE("2005-05-27"),DATE("2005-05-29"));
+
+# SELECT ALL COLUMNS FROM the film TABLE FOR films rated G, PG-13 OR NC-17.
+
+SELECT
+	*
+FROM film
+WHERE rating IN ('G', 'PG-13', 'NC-17');
+
+-- 5. BETWEEN operator
+
+# SELECT ALL COLUMNS FROM the payment TABLE FOR payments made BETWEEN midnight 05/25/2005 AND 1 SECOND BEFORE midnight 05/26/2005.
+# SELECT the following COLUMNS FROM the film TABLE FOR films WHERE the length of the description IS BETWEEN 100 AND 120. Hint: total_rental_cost = rental_duration * rental_rate
+
+-- 6. LIKE operator
+
+# SELECT the following COLUMNS FROM the film TABLE FOR ROWS WHERE the description begins WITH "A Thoughtful".
+# SELECT the following COLUMNS FROM the film TABLE FOR ROWS WHERE the description ENDS WITH the word "Boat".
+# SELECT the following COLUMNS FROM the film TABLE WHERE the description CONTAINS the word "DATABASE" AND the length of the film IS greater THAN 3 hours.
+-- 7. LIMIT Operator
+
+#SELECT ALL COLUMNS FROM the payment TABLE AND only include the FIRST 20 rows.
+#SELECT the payment DATE AND amount COLUMNS FROM the payment TABLE FOR ROWS WHERE the payment amount IS greater THAN 5, AND only SELECT ROWS whose zero-based INDEX IN the result SET IS BETWEEN 1000-2000.
+#SELECT ALL COLUMNS FROM the customer TABLE, limiting results TO those WHERE the zero-based INDEX IS BETWEEN 101-200.
+-- 8. ORDER BY statement
+
+# SELECT ALL COLUMNS FROM the film TABLE AND order ROWS BY the length field IN ascending order.
+# SELECT ALL DISTINCT ratings FROM the film TABLE ordered BY rating IN descending order.
+# SELECT the payment DATE AND amount COLUMNS FROM the payment TABLE FOR the FIRST 20 payments ordered BY payment amount IN descending order.
+# SELECT the title, description, special features, length, AND rental duration COLUMNS FROM the film TABLE FOR the FIRST 10 films WITH behind the scenes footage under 2 hours IN length AND a rental duration BETWEEN 5 AND 7 days, ordered BY length IN descending order.
+-- 9. JOINs
+
+# SELECT customer first_name/last_name AND actor first_name/last_name COLUMNS FROM performing a LEFT JOIN BETWEEN the customer AND actor COLUMN ON the last_name COLUMN IN EACH table. (i.e. customer.last_name = actor.last_name)
+# Label customer first_name/last_name COLUMNS AS customer_first_name/customer_last_name
+# Label actor first_name/last_name COLUMNS IN a similar fashion.
+# RETURNS correct number of records: 599
+# SELECT the customer first_name/last_name AND actor first_name/last_name COLUMNS FROM performing a /RIGHT JOIN BETWEEN the customer AND actor COLUMN ON the last_name COLUMN IN EACH table. (i.e. customer.last_name = actor.last_name)
+# RETURNS correct number of records: 200
+# SELECT the customer first_name/last_name AND actor first_name/last_name COLUMNS FROM performing an INNER JOIN BETWEEN the customer AND actor COLUMN ON the last_name COLUMN IN EACH table. (i.e. customer.last_name = actor.last_name)
+# RETURNS correct number of records: 43
+# SELECT the city NAME AND country NAME COLUMNS FROM the city TABLE, performing a LEFT JOIN WITH the country TABLE TO get the country NAME column.
+# RETURNS correct records: 600
+# SELECT the title, description, RELEASE YEAR, AND LANGUAGE NAME COLUMNS FROM the film TABLE, performing a LEFT JOIN WITH the LANGUAGE TABLE TO get the "language" column.
+# Label the language.name COLUMN AS "language"
+# RETURNS 1000 ROWS
+# SELECT the first_name, last_name, address, address2, city NAME, district, AND postal CODE COLUMNS FROM the staff TABLE, performing 2 LEFT joins WITH the address TABLE THEN the city TABLE TO get the address AND city related columns.
+# RETURNS correct number of ROWS: 2
+
+/* What IS the average replacement cost of a film? Does this CHANGE depending ON the rating of the film?
 
 
++-----------------------+
+| AVG(replacement_cost) |
++-----------------------+
+|             19.984000 |
++-----------------------+
+1 ROW IN SET (2.39 sec)
 
++--------+-----------------------+
+| rating | AVG(replacement_cost) |
++--------+-----------------------+
+| G      |             20.124831 |
+| PG     |             18.959072 |
+| PG-13  |             20.402556 |
+| R      |             20.231026 |
+| NC-17  |             20.137619 |
++--------+-----------------------+
+5 ROWS IN SET (0.09 sec)
+How many different films of EACH genre are IN the DATABASE?
+
+
++-------------+-------+
+| NAME        | count |
++-------------+-------+
+| ACTION      |    64 |
+| Animation   |    66 |
+| Children    |    60 |
+| Classics    |    57 |
+| Comedy      |    58 |
+| Documentary |    68 |
+| Drama       |    62 |
+| Family      |    69 |
+| FOREIGN     |    73 |
+| Games       |    61 |
+| Horror      |    56 |
+| Music       |    51 |
+| NEW         |    63 |
+| Sci-Fi      |    61 |
+| Sports      |    74 |
+| Travel      |    57 |
++-------------+-------+
+16 ROWS IN SET (0.06 sec)
+What are the 5 frequently rented films?
+
+
++---------------------+-------+
+| title               | total |
++---------------------+-------+
+| BUCKET BROTHERHOOD  |    34 |
+| ROCKETEER MOTHER    |    33 |
+| GRIT CLOCKWORK      |    32 |
+| RIDGEMONT SUBMARINE |    32 |
+| JUGGLER HARDLY      |    32 |
++---------------------+-------+
+5 ROWS IN SET (0.11 sec)
+What are the most most profitable films (IN terms of gross revenue)?
+
+
++-------------------+--------+
+| title             | total  |
++-------------------+--------+
+| TELEGRAPH VOYAGE  | 231.73 |
+| WIFE TURN         | 223.69 |
+| ZORRO ARK         | 214.69 |
+| GOODFELLAS SALUTE | 209.69 |
+| SATURDAY LAMBS    | 204.72 |
++-------------------+--------+
+5 ROWS IN SET (0.17 sec)
+Who IS the best customer?
+
+
++------------+--------+
+| NAME       | total  |
++------------+--------+
+| SEAL, KARL | 221.55 |
++------------+--------+
+1 ROW IN SET (0.12 sec)
+Who are the most popular actors (that have appeared IN the most films)?
+
+
++-----------------+-------+
+| actor_name      | total |
++-----------------+-------+
+| DEGENERES, GINA |    42 |
+| TORN, WALTER    |    41 |
+| KEITEL, MARY    |    40 |
+| CARREY, MATTHEW |    39 |
+| KILMER, SANDRA  |    37 |
++-----------------+-------+
+5 ROWS IN SET (0.07 sec)
+What are the sales FOR EACH store FOR EACH MONTH IN 2005?
+
+SELECT
+	concat(YEAR(payment_date), "-", MONTH(payment_date)) AS months,
+	store_id,
+	sum(amount) AS sales
+FROM payment
+JOIN rental USING(rental_id)
+JOIN inventory USING(inventory_id)
+WHERE payment_date LIKE "2005%"
+GROUP BY months, store_id;
+
+
++---------+----------+----------+
+| MONTH   | store_id | sales    |
++---------+----------+----------+
+| 2005-05 |        1 |  2459.25 |
+| 2005-05 |        2 |  2364.19 |
+| 2005-06 |        1 |  4734.79 |
+| 2005-06 |        2 |  4895.10 |
+| 2005-07 |        1 | 14308.66 |
+| 2005-07 |        2 | 14060.25 |
+| 2005-08 |        1 | 11933.99 |
+| 2005-08 |        2 | 12136.15 |
++---------+----------+----------+
+8 ROWS IN SET (0.14 sec)
+Bonus: Find the film title, customer NAME, customer phone number, AND customer address FOR ALL the outstanding DVDs.
+
+
++------------------------+------------------+--------------+
+| title                  | customer_name    | phone        |
++------------------------+------------------+--------------+
+| HYDE DOCTOR            | KNIGHT, GAIL     | 904253967161 |
+| HUNGER ROOF            | MAULDIN, GREGORY | 80303246192  |
+| FRISCO FORREST         | JENKINS, LOUISE  | 800716535041 |
+| TITANS JERK            | HOWELL, WILLIE   | 991802825778 |
+| CONNECTION MICROCOSMOS | DIAZ, EMILY      | 333339908719 |
++------------------------+------------------+--------------+
+5 ROWS IN SET (0.06 sec)
+183 ROWS total, above IS just the FIRST 5
 
 
 
